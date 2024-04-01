c0def53c612a34510f71e0340668eecfc96c396a
Alberto Polo <bertopolo91@gmail.com>
2024-04-01 18:02:29 +0200

    bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
+import 'package:ask_your_food_app/simple_bloc_observer.dart';
+import 'package:ask_your_food_app/src/app.dart';
+import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
+  runApp(MyApp(FirebaseUserRepo()));
 }
